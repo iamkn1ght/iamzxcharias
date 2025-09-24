61068a5d1ec70f5fd200deeb200b887ac0c2e093
zacharias.priller <contact@zachpriller.com>
2025-09-24 20:19:18 +0200

    feature/contact-form-styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
     extend: {
       colors: {
         primary: '#211F1B',
-        secondary: '#37332B', 
+        secondary: '#37332B',
         font: '#D8B577',
         accent: {
           primary: '#D29C24',
           secondary: '#D07D2E',
-        }
+        },
+        'border-light': '#D29C24',
+        'border-dark': '#211F1B',
       },
     },
   },
